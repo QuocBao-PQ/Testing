8ad9fe59c7a85ef169e244ad91ebd9324ec00336
QuocBao-PQ <quocbao7864@gmail.com>
2024-08-31 20:13:32 +0700

    Add files via upload

--- My_Bot.py
+++ My_Bot.py
@@ -148,6 +148,12 @@ def scraping_jobs(scraped_web, num):
     Skills = []
     Job_Link = []
     page = 1
+    
+    chrome_options = uc.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    
     try:
         while page <= int(num):
             page_str = '&page=' + str(page)
@@ -155,7 +161,7 @@ def scraping_jobs(scraped_web, num):
             print (url)
 
             # Navigate to the URL
-            driver = uc.Chrome (version_main=127)
+            driver = uc.Chrome (options=chrome_options)
             driver.get(url)
 
 
